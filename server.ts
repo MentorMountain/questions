5c087c0c62b3ba7efdb523123b95dc2392545742
Kyle Saburao <ksaburao@sfu.ca>
2023-03-09 13:40:35 -0800

    use google suggested defaults for server

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
 import express, { Express } from "express";
 
 const app: Express = express();
-const port = 8080;
-const hostname = process.env.hostname || "0.0.0.0";
+const port = (process.env.PORT && parseInt(process.env.PORT)) || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,39 +13,36 @@ app.use("/", (req, _, next) => {
 
 // Create question
 app.post("/api/questions", (req, res) => {
-    /* Validation
+  /* Validation
       - Ensure responder is a student
   */
 
   res.json("UNIMPLEMENTED");
   // Return created UUID
-
 });
 
 // Get question thread
 app.get("/api/questions/:questionUUID", (req, res) => {
   res.json("UNIMPLEMENTED");
-})
+});
 
 // Respond to question
 app.post("/api/questions/:questionUUID/responses", (req, res) => {
   const uuid = req.params.questionUUID;
-  
+
   /* Validation
     - Ensure question exists
     - Ensure responder is a mentor
   */
-  
 
   res.json("UNIMPLEMENTED");
-})
+});
 
 // Get questions UUIDs
 app.get("/api/questions", (req, res) => {
   const questionUUIDs = [];
   res.json("UNIMPLEMENTED");
-})
-
+});
 
 app.get("/api/health", (_, res) => {
   res.json({
@@ -54,6 +50,6 @@ app.get("/api/health", (_, res) => {
   });
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Attaching to http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Attaching to port ${port}`);
 });
