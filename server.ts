7137aa8f43e709091c38415e5651fa622a6fa5f6
Kyle Saburao <ksaburao@sfu.ca>
2023-03-09 13:16:34 -0800

    add question endpoint boilerplate

--- server.ts
+++ server.ts
@@ -12,6 +12,42 @@ app.use("/", (req, _, next) => {
   next();
 });
 
+// Create question
+app.post("/api/questions", (req, res) => {
+    /* Validation
+      - Ensure responder is a student
+  */
+
+  res.json("UNIMPLEMENTED");
+  // Return created UUID
+
+});
+
+// Get question thread
+app.get("/api/questions/:questionUUID", (req, res) => {
+  res.json("UNIMPLEMENTED");
+})
+
+// Respond to question
+app.post("/api/questions/:questionUUID/responses", (req, res) => {
+  const uuid = req.params.questionUUID;
+  
+  /* Validation
+    - Ensure question exists
+    - Ensure responder is a mentor
+  */
+  
+
+  res.json("UNIMPLEMENTED");
+})
+
+// Get questions UUIDs
+app.get("/api/questions", (req, res) => {
+  const questionUUIDs = [];
+  res.json("UNIMPLEMENTED");
+})
+
+
 app.get("/api/health", (_, res) => {
   res.json({
     health: "OK",
