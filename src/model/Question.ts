2a1398a440a72a4d48bb57367779d8f47df5072e
Kyle Saburao <ksaburao@sfu.ca>
2023-03-09 13:00:45 -0800

    add data models

---
+++ src/model/Question.ts
@@ -0,0 +1,7 @@
+export interface Question {
+    uuid: string;
+    authorUUID: string;
+    date: number;
+    title: string;
+    content: string;
+}
---
+++ src/model/QuestionResponse.ts
@@ -0,0 +1,7 @@
+export interface QuestionResponse {
+  uuid: string;
+  questionUUID: string;
+  authorUUID: string;
+  date: number;
+  message: string;
+}
