677d698f5b6b325d97fa4029db54198c9175bedc
Kyle Saburao <ksaburao@sfu.ca>
2023-03-13 21:54:02 -0700

    cors domain restriction

--- server.ts
+++ server.ts
@@ -17,9 +17,24 @@ const QUESTION_RESPONSES_COLLECTION: string = "QuestionResponses"
 const app: Express = express();
 const port: number = (process.env.PORT && parseInt(process.env.PORT)) || 8080;
 
-app.use(cors({
-  origin: '*'
-}));
+// https://www.npmjs.com/package/cors
+const whitelist = [
+  "https://mentormountain.ca",
+  "https://www.mentormountain.ca",
+  "https://www.sfu.ca"
+];
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
